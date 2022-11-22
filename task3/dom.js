0d361a5697dc922e263b54f1282e62b687681737
vishal2796 <vishal.pb.27@gmail.com>
2022-11-22 17:00:38 +0530

    task 4

--- task3/dom.js
+++ task3/dom.js
@@ -8,3 +8,11 @@ header.style.borderBottom='solid 3px #000'
 const listTitle= document.getElementsByClassName('title')
 listTitle[0].style.fontWeight='bold'
 listTitle[0].style.color='green'
+
+const items= document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor="green"
+
+for(let i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight='bold'
+}
