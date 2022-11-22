4b66b7cfe44d81ade99ffc3c07d7cbf4dae4a6a0
vishal2796 <vishal.pb.27@gmail.com>
2022-11-22 18:57:40 +0530

    querySelectors

--- task3/dom.js
+++ task3/dom.js
@@ -17,10 +17,26 @@ listTitle[0].style.color='green'
 //     items[i].style.fontWeight='bold'
 // }
 
-const items= document.getElementsByTagName('li')
-items[2].style.backgroundColor="green"
+// const items= document.getElementsByTagName('li')
+// items[2].style.backgroundColor="green"
 
-for(let i=0; i<items.length; i++)
+// for(let i=0; i<items.length; i++)
+// {
+//     items[i].style.fontWeight='bold'
+// }
+
+// const secondItem= document.querySelector('.list-group-item:nth-child(2)')
+// secondItem.style.backgroundColor='green'
+
+// const thirdItem= document.querySelector('.list-group-item:nth-child(3)')
+// thirdItem.style.color='white'
+
+const secondItem= document.querySelectorAll('.list-group-item')
+secondItem[1].style.backgroundColor='green'
+
+const odd=document.querySelectorAll('li:nth-child(odd)')
+
+for(let i=0; i<odd.length; i++)
 {
-    items[i].style.fontWeight='bold'
+    odd[i].style.backgroundColor='green'
 }
