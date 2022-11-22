f5d107352b9c9100425cbf56e9d498481b9cd174
vishal2796 <vishal.pb.27@gmail.com>
2022-11-22 18:34:16 +0530

    task 5

--- task3/dom.js
+++ task3/dom.js
@@ -9,7 +9,15 @@ const listTitle= document.getElementsByClassName('title')
 listTitle[0].style.fontWeight='bold'
 listTitle[0].style.color='green'
 
-const items= document.getElementsByClassName('list-group-item')
+// const items= document.getElementsByClassName('list-group-item')
+// items[2].style.backgroundColor="green"
+
+// for(let i=0; i<items.length; i++)
+// {
+//     items[i].style.fontWeight='bold'
+// }
+
+const items= document.getElementsByTagName('li')
 items[2].style.backgroundColor="green"
 
 for(let i=0; i<items.length; i++)
