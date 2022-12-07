05fad001b8e52f3cf039309b3762338b5a9caef9
vishal2796 <vishal.pb.27@gmail.com>
2022-12-07 15:01:25 +0530

    PUT req for appointment

--- main.js
+++ main.js
@@ -41,31 +41,55 @@ function onSubmit(e) {
 	}
 	//SENDING DATA THROUGH POST
 	else {
-		//SENDING POST REQUEST TO CRUD CRUD API
-		Array.from(listData).forEach((item) => {
-			if (emailInput.value == item.childNodes[1].textContent) {
-				userExist = true;
-				msg.classList.add('error');
-				msg.innerHTML = 'This user already has an appointment';
-				setTimeout(() => msg.remove(), 3000);
+		if (!editPos) {
+			//SENDING POST REQUEST TO CRUD CRUD API
+			Array.from(listData).forEach((item) => {
+				if (emailInput.value == item.childNodes[1].textContent) {
+					userExist = true;
+					msg.classList.add('error');
+					msg.innerHTML = 'This user already has an appointment';
+					setTimeout(() => msg.remove(), 3000);
+				}
+			});
+			if (userExist == false) {
+				let userDetails = {
+					name: nameInput.value,
+					email: emailInput.value,
+				};
+				axios
+					.post(
+						' https://crudcrud.com/api/fb24a18a988e461ea9651a842e064a33/appointmentData',
+						userDetails
+					)
+					.then(() => {
+						addItem();
+					})
+					.catch((err) => console.log(err));
 			}
-		});
-		if (userExist == false) {
-			let userDetails = {
+		} else {
+			console.log('inside edit', editPos.childNodes[1].textContent);
+			let element = editPos;
+			let editId;
+			allData.forEach((item) => {
+				if (editPos.childNodes[1].textContent == item.email) editId = item._id;
+			});
+			let updatedDetails = {
 				name: nameInput.value,
 				email: emailInput.value,
 			};
 			axios
-				.post(
-					' https://crudcrud.com/api/fb24a18a988e461ea9651a842e064a33/appointmentData',
-					userDetails
+				.put(
+					`https://crudcrud.com/api/fb24a18a988e461ea9651a842e064a33/appointmentData/${editId}`,
+					updatedDetails
 				)
 				.then(() => {
-					addItem();
+					element.childNodes[0].textContent = `${nameInput.value} `;
+					element.childNodes[1].textContent = emailInput.value;
 				})
-				.catch((err) => console.log(err));
+				.catch((err) => console.log(`Error on edit:${err}`));
 		}
 	}
+	editPos = false;
 	userExist = false;
 	// else{
 	//     if(!editPos){
